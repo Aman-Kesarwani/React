d83684646751d6ae23234a993fe3a46cc7e3f185
Aman-Kesarwani <amank2410@gmail.com>
2021-06-07 10:23:47 +0530

    Adding css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
+import './ExpenseItem.css';
+
+
 function ExpenseItem() {
   return (
-    <div>
+    <div className = "expense-item">
       <div>March 28th 2021</div>
-      <div>
+      <div className ="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>5 lakshs</div>
+        <div className="expense-item__price">5 lakshss</div>
       </div>
     </div>
   );
